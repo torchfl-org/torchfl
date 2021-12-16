4310b1faee0800ea8a1d8e671f31600fe8678388
vivekkhimani <vivekkhimani07@gmail.com>
2021-12-16 11:35:27 -0500

    tox setup

--- tests/test_torchfl.py
+++ tests/test_torchfl.py
@@ -22,3 +22,4 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    assert(torchfl.main(), 0)
--- torchfl/torchfl.py
+++ torchfl/torchfl.py
@@ -1,1 +1,4 @@
 """Main module."""
+
+def main() -> int:
+    return 0
