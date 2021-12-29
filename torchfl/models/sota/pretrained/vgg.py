cbe430841f21f9d97fd4a4b8653890a9e1089ed3
vivekkhimani <vivekkhimani07@gmail.com>
2021-12-28 21:16:20 -0500

    added vgg pretrained implementations

---
+++ torchfl/models/sota/pretrained/vgg.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# type: ignore
+
+"""Implementation of the pre-trained ResNet architectures using PyTorch and torchvision.
+
+Contains:
+    - VGG11
+    - VGG11_BN
+    - VGG13
+    - VGG13_BN
+    - VGG16
+    - VGG16_BN
+    - VGG19
+    - VGG19_BN
+"""
+
+from torchvision import models
+from types import SimpleNamespace
+import torch.nn as nn
+
+
+class VGG11(models.vgg11(pretrained=True, progress=True)):
+    def __init__(
+        self, feature_extract=True, num_classes=10, act_fn_name="relu", **kwargs
+    ) -> None:
+        """Constructor
+
+        Args:
+            feature_extract (bool, optional): Only trains the sequential layers of the pre-trained model. If False, the entire model is finetuned. Defaults to True.
+            num_classes (int, optional): Number of classification outputs. Defaults to 10.
+            act_fn_name (str, optional): Activation function to be used. Defaults to "relu". Accepted: ["tanh", "relu", "leakyrelu", "gelu"].
+        """
+        super(VGG11, self).__init__()
+        self.hparams = SimpleNamespace(
+            model_name="vgg11",
+            pre_trained=True,
+            feature_extract=feature_extract,
+            finetune=bool(not feature_extract),
+            quantized=False,
+            num_classes=num_classes,
+            act_fn_name=act_fn_name,
+            act_fn=ACTIVATION_FUNCTIONS_BY_NAME[act_fn_name],
+        )
+
+        if feature_extract:
+            for param in self.parameters():
+                param.requires_grad = False
+
+        in_features = self.classifier[6].in_features
+        self.classifier[6] = nn.Linear(in_features, self.hparams.num_classes)
+
+
+class VGG11_BN(models.vgg11_bn(pretrained=True, progress=True)):
+    def __init__(
+        self, feature_extract=True, num_classes=10, act_fn_name="relu", **kwargs
+    ) -> None:
+        """Constructor
+
+        Args:
+            feature_extract (bool, optional): Only trains the sequential layers of the pre-trained model. If False, the entire model is finetuned. Defaults to True.
+            num_classes (int, optional): Number of classification outputs. Defaults to 10.
+            act_fn_name (str, optional): Activation function to be used. Defaults to "relu". Accepted: ["tanh", "relu", "leakyrelu", "gelu"].
+        """
+        super(VGG11_BN, self).__init__()
+        self.hparams = SimpleNamespace(
+            model_name="vgg11_bn",
+            pre_trained=True,
+            feature_extract=feature_extract,
+            finetune=bool(not feature_extract),
+            quantized=False,
+            num_classes=num_classes,
+            act_fn_name=act_fn_name,
+            act_fn=ACTIVATION_FUNCTIONS_BY_NAME[act_fn_name],
+        )
+
+        if feature_extract:
+            for param in self.parameters():
+                param.requires_grad = False
+
+        in_features = self.classifier[6].in_features
+        self.classifier[6] = nn.Linear(in_features, self.hparams.num_classes)
+
+
+class VGG13(models.vgg13(pretrained=True, progress=True)):
+    def __init__(
+        self, feature_extract=True, num_classes=10, act_fn_name="relu", **kwargs
+    ) -> None:
+        """Constructor
+
+        Args:
+            feature_extract (bool, optional): Only trains the sequential layers of the pre-trained model. If False, the entire model is finetuned. Defaults to True.
+            num_classes (int, optional): Number of classification outputs. Defaults to 10.
+            act_fn_name (str, optional): Activation function to be used. Defaults to "relu". Accepted: ["tanh", "relu", "leakyrelu", "gelu"].
+        """
+        super(VGG13, self).__init__()
+        self.hparams = SimpleNamespace(
+            model_name="vgg13",
+            pre_trained=True,
+            feature_extract=feature_extract,
+            finetune=bool(not feature_extract),
+            quantized=False,
+            num_classes=num_classes,
+            act_fn_name=act_fn_name,
+            act_fn=ACTIVATION_FUNCTIONS_BY_NAME[act_fn_name],
+        )
+
+        if feature_extract:
+            for param in self.parameters():
+                param.requires_grad = False
+
+        in_features = self.classifier[6].in_features
+        self.classifier[6] = nn.Linear(in_features, self.hparams.num_classes)
+
+
+class VGG13_BN(models.vgg13_bn(pretrained=True, progress=True)):
+    def __init__(
+        self, feature_extract=True, num_classes=10, act_fn_name="relu", **kwargs
+    ) -> None:
+        """Constructor
+
+        Args:
+            feature_extract (bool, optional): Only trains the sequential layers of the pre-trained model. If False, the entire model is finetuned. Defaults to True.
+            num_classes (int, optional): Number of classification outputs. Defaults to 10.
+            act_fn_name (str, optional): Activation function to be used. Defaults to "relu". Accepted: ["tanh", "relu", "leakyrelu", "gelu"].
+        """
+        super(VGG13_BN, self).__init__()
+        self.hparams = SimpleNamespace(
+            model_name="vgg13_bn",
+            pre_trained=True,
+            feature_extract=feature_extract,
+            finetune=bool(not feature_extract),
+            quantized=False,
+            num_classes=num_classes,
+            act_fn_name=act_fn_name,
+            act_fn=ACTIVATION_FUNCTIONS_BY_NAME[act_fn_name],
+        )
+
+        if feature_extract:
+            for param in self.parameters():
+                param.requires_grad = False
+
+        in_features = self.classifier[6].in_features
+        self.classifier[6] = nn.Linear(in_features, self.hparams.num_classes)
+
+
+class VGG16(models.vgg16(pretrained=True, progress=True)):
+    def __init__(
+        self, feature_extract=True, num_classes=10, act_fn_name="relu", **kwargs
+    ) -> None:
+        """Constructor
+
+        Args:
+            feature_extract (bool, optional): Only trains the sequential layers of the pre-trained model. If False, the entire model is finetuned. Defaults to True.
+            num_classes (int, optional): Number of classification outputs. Defaults to 10.
+            act_fn_name (str, optional): Activation function to be used. Defaults to "relu". Accepted: ["tanh", "relu", "leakyrelu", "gelu"].
+        """
+        super(VGG16, self).__init__()
+        self.hparams = SimpleNamespace(
+            model_name="vgg16",
+            pre_trained=True,
+            feature_extract=feature_extract,
+            finetune=bool(not feature_extract),
+            quantized=False,
+            num_classes=num_classes,
+            act_fn_name=act_fn_name,
+            act_fn=ACTIVATION_FUNCTIONS_BY_NAME[act_fn_name],
+        )
+
+        if feature_extract:
+            for param in self.parameters():
+                param.requires_grad = False
+
+        in_features = self.classifier[6].in_features
+        self.classifier[6] = nn.Linear(in_features, self.hparams.num_classes)
+
+
+class VGG16_BN(models.vgg16_bn(pretrained=True, progress=True)):
+    def __init__(
+        self, feature_extract=True, num_classes=10, act_fn_name="relu", **kwargs
+    ) -> None:
+        """Constructor
+
+        Args:
+            feature_extract (bool, optional): Only trains the sequential layers of the pre-trained model. If False, the entire model is finetuned. Defaults to True.
+            num_classes (int, optional): Number of classification outputs. Defaults to 10.
+            act_fn_name (str, optional): Activation function to be used. Defaults to "relu". Accepted: ["tanh", "relu", "leakyrelu", "gelu"].
+        """
+        super(VGG16_BN, self).__init__()
+        self.hparams = SimpleNamespace(
+            model_name="vgg16_bn",
+            pre_trained=True,
+            feature_extract=feature_extract,
+            finetune=bool(not feature_extract),
+            quantized=False,
+            num_classes=num_classes,
+            act_fn_name=act_fn_name,
+            act_fn=ACTIVATION_FUNCTIONS_BY_NAME[act_fn_name],
+        )
+
+        if feature_extract:
+            for param in self.parameters():
+                param.requires_grad = False
+
+        in_features = self.classifier[6].in_features
+        self.classifier[6] = nn.Linear(in_features, self.hparams.num_classes)
+
+
+class VGG19(models.vgg19(pretrained=True, progress=True)):
+    def __init__(
+        self, feature_extract=True, num_classes=10, act_fn_name="relu", **kwargs
+    ) -> None:
+        """Constructor
+
+        Args:
+            feature_extract (bool, optional): Only trains the sequential layers of the pre-trained model. If False, the entire model is finetuned. Defaults to True.
+            num_classes (int, optional): Number of classification outputs. Defaults to 10.
+            act_fn_name (str, optional): Activation function to be used. Defaults to "relu". Accepted: ["tanh", "relu", "leakyrelu", "gelu"].
+        """
+        super(VGG19, self).__init__()
+        self.hparams = SimpleNamespace(
+            model_name="vgg19",
+            pre_trained=True,
+            feature_extract=feature_extract,
+            finetune=bool(not feature_extract),
+            quantized=False,
+            num_classes=num_classes,
+            act_fn_name=act_fn_name,
+            act_fn=ACTIVATION_FUNCTIONS_BY_NAME[act_fn_name],
+        )
+
+        if feature_extract:
+            for param in self.parameters():
+                param.requires_grad = False
+
+        in_features = self.classifier[6].in_features
+        self.classifier[6] = nn.Linear(in_features, self.hparams.num_classes)
+
+
+class VGG19_BN(models.vgg19_bn(pretrained=True, progress=True)):
+    def __init__(
+        self, feature_extract=True, num_classes=10, act_fn_name="relu", **kwargs
+    ) -> None:
+        """Constructor
+
+        Args:
+            feature_extract (bool, optional): Only trains the sequential layers of the pre-trained model. If False, the entire model is finetuned. Defaults to True.
+            num_classes (int, optional): Number of classification outputs. Defaults to 10.
+            act_fn_name (str, optional): Activation function to be used. Defaults to "relu". Accepted: ["tanh", "relu", "leakyrelu", "gelu"].
+        """
+        super(VGG19_BN, self).__init__()
+        self.hparams = SimpleNamespace(
+            model_name="vgg19_bn",
+            pre_trained=True,
+            feature_extract=feature_extract,
+            finetune=bool(not feature_extract),
+            quantized=False,
+            num_classes=num_classes,
+            act_fn_name=act_fn_name,
+            act_fn=ACTIVATION_FUNCTIONS_BY_NAME[act_fn_name],
+        )
+
+        if feature_extract:
+            for param in self.parameters():
+                param.requires_grad = False
+
+        in_features = self.classifier[6].in_features
+        self.classifier[6] = nn.Linear(in_features, self.hparams.num_classes)
