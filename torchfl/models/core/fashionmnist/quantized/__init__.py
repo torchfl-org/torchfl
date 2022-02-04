dd518371f683d5f93c1193aade0b64b901b11932
vivekkhimani <vivekkhimani07@gmail.com>
2022-02-04 10:57:14 -0500

    quantized models scoped out.

--- torchfl/models/core/fashionmnist/quantized/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Sub-package for pre-trained and quantized models for FashionMNIST model implementations provided by torchfl."""
-
-__author__ = """Vivek Khimani"""
-__email__ = "vivekkhimani07@gmail.com"
-__version__ = "0.1.0"
