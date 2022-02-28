2655a4f102ec27a45fd0c672be2ebbb08643474b
vivekkhimani <vivekkhimani07@gmail.com>
2022-02-28 12:40:52 -0500

    init federated sub-package

---
+++ torchfl/federated/samplers/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Sub-package for federated learning samplers used by torchfl."""
+
+__author__ = """Vivek Khimani"""
+__email__ = "vivekkhimani07@gmail.com"
+__version__ = "0.1.0"
