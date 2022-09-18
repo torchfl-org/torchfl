f70836217f0e1bf1c8c5075c39175cfe695ef0b0
vivekkhimani <vivekkhimani07@gmail.com>
2022-09-17 17:22:27 -0700

    [release] doc changes and dependency conflicts

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/vivekkhimani/torchfl",
-    version="0.1.2",
+    version="0.1.3",
     zip_safe=False,
 )
